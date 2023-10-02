1d0e0065920c7287415a8f5daca69dcfe59b9743
rogercerpa <roger.e.cerpa@gmail.com>
2023-10-01 22:43:17 -0400

    configured clerk

---
+++ invecars/app/sign-up/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import SignUpForm from "@/components/auth/SignUpForm";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4">
+      <SignUpForm />
+      <div className="flex flex-row items-center gap-1">
+        <p>Have an account already?</p>
+        <Link href="/sign-in" className="text-blue-600 font-bold">
+          Sign In
+        </Link>
+      </div>
+    </div>
+  );
+}
