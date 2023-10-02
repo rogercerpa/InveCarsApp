2472de90df0ddcd715f16b425cea8a81d6140fe5
rogercerpa <roger.e.cerpa@gmail.com>
2023-10-01 22:49:43 -0400

    configured sign in page

---
+++ invecars/app/sign-in/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import SignInForm from "@/components/auth/SignInForm";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4">
+      <SignInForm />
+      <div className="flex flex-row items-center gap-1">
+        <p>Don't have an account?</p>
+        <Link href="/sign-up" className="text-blue-600 font-bold">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ invecars/components/auth/SignInForm.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useSignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+interface SignUpFormValues {
+  email: string;
+  password: string;
+}
+
+export default function SignInForm() {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<SignUpFormValues>();
+  const { signIn, setActive } = useSignIn();
+
+  const onSubmit = async ({ email, password }: SignUpFormValues) => {
+    try {
+      if (!signIn) {
+        // eslint-disable-next-line no-console
+        console.log("Clerk sign in not avaialble");
+        return null;
+      }
+
+      const response = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      await setActive({ session: response.createdSessionId });
+      router.push("/");
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  };
+
+  return (
+    <form
+      className="flex flex-col items-center justify-center w-full"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-4 w-full">
+        <h1 className="text-2xl font-bold">Sign In</h1>
+        <div className="flex flex-col">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="text"
+            {...register("email", { required: true })}
+            className="text-lg p-2 px-3 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            className="text-lg p-2 px-3 rounded-md"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-400 text-white font-medium py-2 rounded-md"
+        >
+          Continue
+        </button>
+      </div>
+    </form>
+  );
+}
